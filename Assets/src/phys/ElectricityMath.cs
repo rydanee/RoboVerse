35a3709f426091b0df471ca707bae7b63b9cdd8f
qezem <nikikamir0@gmail.com>
2026-07-28 08:47:25 +0500

    Implement more physics formulas in ElectricityMath.cs

--- Assets/src/phys/ElectricityMath.cs
+++ Assets/src/phys/ElectricityMath.cs
@@ -15,22 +15,93 @@ namespace phys
       return true;
     }
 
-    public static float Calculate_I_By_Ohms_Law(float U, float R)
+    public static bool checkI(float I)
+    {
+      if (I == 0f)
+      {
+        Debug.Log("Ток равен нулю!");
+        return false;
+      }
+
+      return true;
+    }
+
+    //Current formulas
+
+    public static float Calculate_I(float U, float R)
     {
       if (!checkR(R)) return -1f;
       return U / R;
     }
 
-    public static float Calculate_U_By_Ohms_Law(float I, float R)
+    public static float Calculate_I(float P, float U)
+    {
+      return P / U;
+    }
+
+    public static float Calculate_I(float P, float R)
+    {
+      if (!checkR(R)) return -1f;
+      return Mathf.Sqrt(P / R);
+    }
+
+    //Voltage formulas
+
+    public static float Calculate_U(float I, float R)
     {
       if (!checkR(R)) return -1f;
       return I * R;
     }
 
-    public static float Calculate_R_By_Ohms_Law(float U, float I)
+    public static float Calculate_U(float P, float I)
+    {
+      if (!checkI(I)) return -1f;
+      return P / I;
+    }
+
+    public static float Calculate_U(float P, float R)
     {
       if (!checkR(R)) return -1f;
+      return Mathf.Sqrt(P * R);
+    }
+
+    //Resistance formulas
+
+    public static float Calculate_R(float U, float I)
+    {
+      if (!checkI(I)) return -1f;
       return U / I;
     }
+
+    public static float Calculate_R(float U, float P)
+    {
+      return Mathf.Pow(U, 2) / P;
+    }
+
+    public static float Calculate_R(float P, float I)
+    {
+      if (!checkI(I)) return -1f;
+      return P / Mathf.Pow(I, 2);
+    }
+
+    //Power formulas
+
+    public static float Calculate_P(float U, float I)
+    {
+      return U * I;
+    }
+
+    public static float Calculate_P(float R, float I)
+    {
+      if (!checkR(R)) return -1f;
+      return R * Mathf.Pow(I, 2);
+    }
+
+    public static float Calculate_P(float U, float R)
+    {
+      if (!checkR(R)) return -1f;
+      return Mathf.Pow(U, 2) / R;
+    }
+
   }
 }
