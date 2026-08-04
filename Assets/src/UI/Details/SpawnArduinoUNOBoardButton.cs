c7a3118312ef7fbe98851d39c038ba77446a68bc
qezem <nikikamir0@gmail.com>
2026-08-04 09:33:12 +0500

    Fix Arduino UNO board spawner

--- Assets/src/UI/Details/SpawnArduinoUNOBoardButton.cs
+++ Assets/src/UI/Details/SpawnArduinoUNOBoardButton.cs
@@ -8,9 +8,12 @@ public class SpawnArduinoUNOBoardButton : MonoBehaviour
   {
     Camera camera = CanvasController.Instance.camera;
 
-    Vector3 spawnPos = camera.transform.position + (camera.transform.forward * 2f);
+    Vector3 spawnPos = camera.transform.position;
+    spawnPos.y = 2.0f;
 
     Instantiate(arduinoUNOBoardObject, spawnPos, arduinoUNOBoardObject.transform.rotation * Quaternion.Euler(0, 0, 180f));
+
+    Debug.Log(arduinoUNOBoardObject.transform.position.y);
 
     CanvasController.Instance.toggleCanvas(CanvasController.Instance.defaultButtonText);
 
